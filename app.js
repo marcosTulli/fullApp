296db518f2c20dfa5e7caf3a4f88e73bcf34e085
Marcos Tulli <marcos.tulli@raona.com>
2023-08-01 15:54:02 -0300

    Add: /public

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
   res.send('Hello From my app');
