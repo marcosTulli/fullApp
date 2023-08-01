d8f03bf2e6adfecbd8c26c4e808a8755ed08a168
Marcos Tulli <marcos.tulli@raona.com>
2023-08-01 15:49:15 -0300

    Add utils libraries

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+const port = 3000;
+
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello From my app');
 });
 
 //Comment
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.green(port)} `);
 });
