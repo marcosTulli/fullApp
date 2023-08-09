4c8ca1d7a6c46f6cc1fa9aff1e76e8faed279201
Marcos Tulli <marcos.tulli@raona.com>
2023-08-09 20:49:56 -0300

    Add: pull single id

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,19 +1,52 @@
 const express = require('express');
+const chalk = require('chalk');
 const debug = require('debug')('app:sessionsRouter');
-const { MongoClient } = require('mongodb');
-const sessions = require('../data/sessions.json');
+const { MongoClient, ObjectId } = require('mongodb');
+// const sessions = require('../data/sessions.json');
 
 const sessionRouter = express.Router();
 
 sessionRouter.route('/').get((req, res) => {
-  res.render('sessions', {
-    sessions,
-  });
+  const url = 'mongodb+srv://marcosTulli:tuki@cluster0.hkb8byd.mongodb.net/?retryWrites=true&w=majority';
+  const dbName = 'globomantics';
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+
+      debug(`${chalk.green('Connected to the mongo DB')}`);
+
+      const db = client.db(dbName);
+      const sessions = await db.collection('sessions').find().toArray();
+
+      res.render('sessions', { sessions });
+    } catch (error) {
+      debug(error.stack);
+    }
+    client.close();
+  })();
 });
 
 sessionRouter.route('/:id').get((req, res) => {
   const id = req.params.id;
-  res.render('session', { session: sessions[id] });
+  const url = 'mongodb+srv://marcosTulli:tuki@cluster0.hkb8byd.mongodb.net/?retryWrites=true&w=majority';
+  const dbName = 'globomantics';
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected to the mongo DB');
+
+      const db = client.db(dbName);
+      const session = await db.collection('sessions').findOne({ _id: new ObjectId(id) });
+      res.render('session', { session });
+    } catch (error) {
+      debug(error.stack);
+    }
+    client.close();
+  })();
 });
 
 module.exports = sessionRouter;
