92ad5f738a2579ebdcba69a4433c77fa3d28079d
Marcos <marcos.tulli@raona.com>
2023-08-08 23:51:29 -0300

    Add: Mock data

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 sessionRouter.route('/').get((req, res) => {
-  res.send('Hello sessions');
+  res.render('sesisons', {
+    sessions: [
+      { title: 'Session 1', description: 'This is session 1' },
+      { title: 'Session 2', description: 'This is session 2' },
+      { title: 'Session 3', description: 'This is session 3' },
+      { title: 'Session 4', description: 'This is session 4' },
+      { title: 'Session 5', description: 'This is session 5' },
+    ],
+  });
 });
 
 sessionRouter.route('/1').get((req, res) => {
