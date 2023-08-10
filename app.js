1c5bc432bb2b7c4fadc3ccf741ef2078ae930eef
Marcos Tulli <marcos.tulli@raona.com>
2023-08-09 21:27:03 -0300

    Add: Login config

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
+const authRouter = require('./src/routers/authRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, './public/')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/sessions', sessionRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Welcome to Globomantics', data: ['a', 'b', 'c'] });
